55196e23bb88b37d8925a53e8d7ceeda20b89487
Gregory <gkiar@bridges>
2022-11-07 14:05:42 -0500

    added table schema and malformed txt loader

--- code/build_df.py
+++ code/build_df.py
@@ -11,11 +11,25 @@ import numpy as np
 atlas_lut = {
     'cc2': 200,
     'des': 70,
-    'aal': 108,
+    'aal': 116,
     'hox': 54,
 }
 
-def extract_info(path: Path, N: int):
+schema = pa.schema({
+    'dataset': pa.string(),
+    'subject': pa.string(),
+    'session': pa.string(),
+    'alignment': pa.string(),
+    'filtering': pa.bool_(),
+    'scrubbing': pa.bool_(),
+    'gsr': pa.bool_(),
+    'parcellation': pa.string(),
+    'pipeline': pa.string(),
+    'graph': pa.array(),
+    'path': pa.string()
+})
+
+def extract_info(path: Path, N: int) -> dict:
     sub = path.stem.split('_')
     par = path.parent.stem
     df = {
@@ -34,23 +48,32 @@ def extract_info(path: Path, N: int):
     return df
 
 
-def grab_mat(path: Path, N: int, dtype:type = np.float32):
+def dummy_loadtxt(path: Path) -> np.array:
     try:
-        elist = np.loadtxt(path)
-        m = np.zeros((N, N))
-        for e in range(elist.shape[0]):
-            m[int(elist[e,0]), int(elist[e,1])] = elist[e,2]
-        locs = np.triu_indices(N)
-        return m[locs].astype(dtype)
+        return np.loadtxt(path)
     except ValueError as e:
-        return None
+        with open(path, 'r') as fhandle:
+            edges = fhandle.readlines()
+            return np.array([tuple(e.split()) 
+                             if len(e.split() == 3)
+                             else tuple(*e.split(), 0)
+                             for e in edges]).astype(float)
+
+
+def grab_mat(path: Path, N: int, dtype:type = np.float32) -> np.array:
+    elist = dummy_loadtxt(path)
+    m = np.zeros((N, N))
+    for e in range(elist.shape[0]):
+        m[int(elist[e,0]), int(elist[e,1])] = elist[e,2]
+    locs = np.triu_indices(N)
+    return m[locs].astype(dtype)
 
 
 def main():
     parser = ArgumentParser()
     parser.add_argument("dataset_path")
     parser.add_argument("output_loc")
-    parser.add_argument("--parcellation", "-p", default='cc2',
+    parser.add_argument("--parcellation", "-p", default='aal',
                         choices=['cc2', 'hox', 'aal', 'des'])
 
     # Basic parameter setup
@@ -66,8 +89,7 @@ def main():
                if f.parent.match("*{0}*".format(parc))]
 
     # Write to file
-    df = pa.Table.from_pandas(pd.DataFrame.from_dict(df_dict),
-                              preserve_index=False)
+    df = pa.Table.from_pylist(df_dict,schema=schema)
     ofname = results.output_loc + ".parquet"
     pq.write_table(df, ofname, compression='gzip')
 
