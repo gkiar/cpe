98167bf1acc12d38bf4497380dc4ee645a3d1588
Gregory <gkiar@bridges>
2022-11-10 16:18:25 -0500

    added parallelism for deploying across the complete dataset

--- code/build_df.py
+++ code/build_df.py
@@ -1,11 +1,13 @@
 #!/usr/bin/env python
 
+from concurrent.futures import ProcessPoolExecutor, as_completed
 from argparse import ArgumentParser
 from pathlib import Path
 import pyarrow.parquet as pq
 import pyarrow as pa
 import pandas as pd
 import numpy as np
+import os
 
 
 atlas_lut = {
@@ -15,6 +17,7 @@ atlas_lut = {
     'hox': 54,
 }
 
+# TODO: figure out how to include arrays in the schema
 schema = pa.schema({
     'dataset': pa.string(),
     'subject': pa.string(),
@@ -25,7 +28,7 @@ schema = pa.schema({
     'gsr': pa.bool_(),
     'parcellation': pa.string(),
     'pipeline': pa.string(),
-    'graph': pa.array(),
+#     'graph': pa.array(),
     'path': pa.string()
 })
 
@@ -55,8 +58,8 @@ def dummy_loadtxt(path: Path) -> np.array:
         with open(path, 'r') as fhandle:
             edges = fhandle.readlines()
             return np.array([tuple(e.split()) 
-                             if len(e.split() == 3)
-                             else tuple(*e.split(), 0)
+                             if len(e.split()) == 3
+                             else tuple([*e.split(), 0])
                              for e in edges]).astype(float)
 
 
@@ -69,29 +72,53 @@ def grab_mat(path: Path, N: int, dtype:type = np.float32) -> np.array:
     return m[locs].astype(dtype)
 
 
+def search_and_construct(dset: Path, dname: str, output: Path,
+                         parcellation: str="aal") -> Path:
+    # Grab filelist & extract info
+    fls = list(Path(dset).rglob("*.ssv"))
+
+    N = atlas_lut[parcellation]
+    df_dict = [extract_info(f, N) for f in fls]
+
+    # Write to file
+    df = pa.Table.from_pylist(df_dict)
+    ofname = output / (str(dset.stem) + ".parquet")
+    print(ofname)
+    pq.write_table(df, ofname, compression='gzip')
+    return ofname
+
+
 def main():
     parser = ArgumentParser()
     parser.add_argument("dataset_path")
-    parser.add_argument("output_loc")
+    parser.add_argument("output_path")
     parser.add_argument("--parcellation", "-p", default='aal',
                         choices=['cc2', 'hox', 'aal', 'des'])
 
     # Basic parameter setup
     results = parser.parse_args()
-    dset = results.dataset_path
+    dset = Path(results.dataset_path)
+    oloc = Path(results.output_path)
     parc = results.parcellation
 
-    # Grab filelist & extract info
-    fls = list(Path(dset).rglob("*.ssv"))
+    futures, dbpaths = [], []
 
-    N = atlas_lut[parc]
-    df_dict = [extract_info(f, N) for f in fls
-               if f.parent.match("*{0}*".format(parc))]
+    with ProcessPoolExecutor(max_workers=8) as pool:
+        for d in os.listdir(dset):
+            for p in os.listdir(dset / d / "graphs"):
+                if p.endswith('.rds') or parc not in p:
+                    continue
+                datadir = dset / d / "graphs" / p
+                outdir = oloc / d
+                outdir.mkdir(exist_ok = True)
 
-    # Write to file
-    df = pa.Table.from_pylist(df_dict,schema=schema)
-    ofname = results.output_loc + ".parquet"
-    pq.write_table(df, ofname, compression='gzip')
+                futures.append(pool.submit(search_and_construct,
+                                           datadir, d, outdir, parc))
+
+    for future in as_completed(futures):
+        dbpaths.append(future.result())
+
+    print(dbpaths)
 
 
 if __name__ == "__main__":
